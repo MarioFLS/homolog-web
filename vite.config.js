b89cf2b61964bce88b9aecc80eae119a725f7996
MarioFLS <mariofernandolsantos@gmail.com>
2023-08-07 10:10:49 -0300

    feat: add new Build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
-  }
-})
+    'process.env': {},
+  },
+  build: {
+    target: 'esnext', // browsers can handle the latest ES features
+  },
+});
